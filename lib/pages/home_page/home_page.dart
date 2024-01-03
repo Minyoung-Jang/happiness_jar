2d32f35ca35dc5254df69a6f97ba573b5a2e58a0
Minyoung-Jang <myimp9721@gmail.com>
2024-01-03 23:33:30 +0900

    feat: 쪽지 떨어뜨리기 애니메이션 테스팅

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isTapped = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: InkWell(
+        highlightColor: Colors.transparent,
+        splashColor: Colors.transparent,
+        onTap: () {
+          if (isTapped != true) {
+            setState(() {
+              isTapped = true;
+            });
+          }
+        },
+        child: Center(
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/jar.png"),
+                fit: BoxFit.contain,
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    margin: EdgeInsets.only(bottom: isTapped ? 80 : 300),
+                    duration: const Duration(milliseconds: 1200),
+                    curve: Curves.bounceOut,
+                    height: 150,
+                    width: 220,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage('assets/images/note1.png'))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
